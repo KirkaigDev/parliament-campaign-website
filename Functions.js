1d1644939a655b3334e923c76bfa109c359a815b
Kirkaig-YT <mynameisthunder678@gmail.com>
2025-03-23 19:20:07 +1100

    Delete

--- Functions.js
+++
@@ -1,8 +0,0 @@
-function sidebar() {
-    var checkBox = document.getElementById("Test-check");
-    if (checkBox.checked == true){
-      document.getElementById("mySidenav").style.width = "250px";
-} else {
-  document.getElementById("mySidenav").style.width = "0";
-}
-  }
