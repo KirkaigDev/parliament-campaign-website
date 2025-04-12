2a2738951f43cbd3647737f250c792821c59b30e
Kirkaig-YT <mynameisthunder678@gmail.com>
2025-04-12 12:15:03 +1000

    FAQ first iteration

--- Functions.js
+++ Functions.js
@@ -1,8 +1,17 @@
 function sidebar() {
-    var checkBox = document.getElementById("Test-check");
-    if (checkBox.checked == true){
-      document.getElementById("mySidenav").style.width = "100%";
-} else {
-  document.getElementById("mySidenav").style.width = "0";
-}
+  var checkBox = document.getElementById("Test-check");
+  if (checkBox.checked == true) {
+    document.getElementById("mySidenav").style.width = "100%";
+  } else {
+    document.getElementById("mySidenav").style.width = "0";
   }
+}
+
+function dropdown() {
+  var checkBox = document.getElementById("Test-check");
+  if (checkBox.checked == true) {
+    document.getElementById("mySidenav").style.transform = "rotate(90deg)";
+  } else {
+    document.getElementById("mySidenav").style.transform = "rotate(0deg)";
+  }
+}
